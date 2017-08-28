c158a6140dd9216ee027df3bcd198f34faf80b44
naufaldi <naufaldi.rafif@gmail.com>
2017-08-28 11:57:40 +0700

    react webinarr codepolitan

---
+++ webinar-codepolitan/learboard/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Leaderboard extends React.Component{
+  render(){
+    return <h1> Leaderboard</h1>
+  }
+}
+ReactDOM.render(
+  <Leaderboard />,
+  document.getElementById('root')
+);
